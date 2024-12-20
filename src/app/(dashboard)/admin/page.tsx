39cbebd3deb5a1b678bf25024ee39b54089f6609
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2024-12-20 23:18:20 +0600

    feat: add UserCard, AttendanceChart, and CountChart components; update package.json dependencies

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,35 @@
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard"
+
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
-  )
+    <div className="p-4 flex flex-col md:flex-row gap-4">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHART */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className=""></div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 }
 
 export default AdminPage
