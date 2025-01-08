f328558ee723855f8b02d79ede2cf9b5ade71091
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-01-08 21:45:53 +0600

    refactor: update layout and add login/register pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="/school.png"/>
+        <link rel="shortcut icon" href="/logo.png"/>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
