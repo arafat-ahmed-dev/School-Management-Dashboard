6fb22220fbc04ffb707c4357d76318a8d2fa71b2
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-01-24 23:50:53 +0600

    refactor: rename getUserRole to useUserRole for improved clarity

--- src/lib/getUserType.ts
+++ src/lib/getUserType.ts
@@ -2,10 +2,10 @@
 import _ from "lodash";
 import { useAppSelector } from "./store/hooks";
 
-const getUserRole = () => {
+const useUserRole = () => {
   const response = useAppSelector((state) => state.auth.userData?.userRole);
   const role = _.toLower(response);
   return role;
 };
 
-export default getUserRole;
+export default useUserRole;
