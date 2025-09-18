37974bd520437a365515186d780bd38adc318cc5
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-09-18 17:04:10 +0600

    feat: Enhance password hashing and verification with secret management

--- src/lib/argon2.ts
+++ src/lib/argon2.ts
@@ -1,9 +1,12 @@
 import argon2 from "argon2";
 
 export async function hashPassword(password: string) {
-  return await argon2.hash(password);
+  const secret = process.env.ARGON2_SECRET!;
+  return await argon2.hash(password, { secret: Buffer.from(secret) });
 }
 
 export async function verifyPassword(password: string, hash: string) {
-  return await argon2.verify(hash, password);
+    const secret = process.env.ARGON2_SECRET!;
+  return await argon2.verify(hash, password, { secret: Buffer.from(secret) });
 }
+
